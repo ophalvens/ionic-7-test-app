5304f87cb9333c686e58ac53723cafebf157751d
Steven Ophalvens <ophalvens@users.noreply.github.com>
2023-10-16 19:53:26 +0200

    Demo reactiviteit (product)

--- src/views/TabNieuwPage.vue
+++ src/views/TabNieuwPage.vue
@@ -8,7 +8,7 @@
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
+          <ion-title size="large">Voeg een product toe</ion-title>
         </ion-toolbar>
       </ion-header>
       <ion-grid>
@@ -23,26 +23,29 @@
                 label="Product"
                 label-placement="stacked"
                 placeholder="Naam product" 
-                id="productNaam"
-                name="productNaam"
-                required="true"></ion-input>
+                v-model="productNaam"
+                required="true"
+                @ion-blur="logProduct()"></ion-input>
             </ion-item>
             <ion-item>
               <ion-input 
                 label="Prijs"
                 label-placement="stacked"
-                placeholder="10" 
-                id="productPrijs"
-                name="productPrijs"
+                placeholder="10.00" 
+                v-model="productPrijs"
                 type="number"
-                required="true"></ion-input>
+                required="true"
+                @ion-blur="logProduct()"></ion-input>
             </ion-item>
             <ion-item>
               <ion-select
                 label="Categorie"
                 label-placement="stacked" 
                 interface="popover" 
-                placeholder="Selecteer categorie">
+                placeholder="Selecteer categorie"
+                v-model="productCategorie"
+                @ion-change="logProduct()">
+                <ion-select-option value="0" disabled="true">Welk type product?</ion-select-option>
                 <ion-select-option value="1">Fruit</ion-select-option>
                 <ion-select-option value="2">Groente</ion-select-option>
               </ion-select>
@@ -51,7 +54,23 @@
         </ion-row>
         <ion-row>
           <ion-col>
-            <ion-button expand="block">Bevestig</ion-button>
+            <ion-button expand="block" @click="verzendProduct()">Bevestig</ion-button>
+          </ion-col>
+        </ion-row>
+
+        <!-- Het volgende deel staat hier enkel als DEMO voor de reactiviteit in Vue -->
+        <!-- Het is niet nodig voor jouw project, maar je kan het gebruiken om de reactiviteit te testen -->
+        <ion-row>
+          <ion-col>
+            <ion-item>
+              <ion-label>Product: {{productNaam}}</ion-label>
+            </ion-item>
+            <ion-item>
+              <ion-label>Prijs: {{productPrijs}}</ion-label>
+            </ion-item>
+            <ion-item>
+              <ion-label>Categorienummer: {{productCategorie}}</ion-label>
+            </ion-item>
           </ion-col>
         </ion-row>
       </ion-grid>
@@ -61,6 +80,20 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonItem, IonLabel, IonInput, IonSelect, IonSelectOption, IonButton } from '@ionic/vue';
+
+const productNaam = ref( '' );
+const productPrijs = ref( 0.00 );
+const productCategorie = ref( 0 );
+
+const logProduct = () => {
+  console.log(`naam: ${productNaam.value}, prijs: ${productPrijs.value}, categorie: ${productCategorie.value}`);
+};
+
+const verzendProduct = () => {
+  // TODO: dit verder uitwerken en effectief het product verzenden
+  logProduct();
+};
 
 </script>
